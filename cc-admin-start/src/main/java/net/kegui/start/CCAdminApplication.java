9a5ec823915d1004fb41629e4459aaa10beb8554
WANG,ZHEN <admin@5ibug.net>
2025-07-02 13:59:16 +0800

    feat: 新增core模块,处理统一返回包裹和异常

---
+++ cc-admin-start/src/main/java/net/kegui/start/CCAdminApplication.java
@@ -0,0 +1,11 @@
+package net.kegui.start;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CCAdminApplication.class, args);
+    }
+}
---
+++ cc-admin-start/src/main/java/net/kegui/start/controller/TestController.java
@@ -0,0 +1,22 @@
+package net.kegui.start.controller;
+
+import net.kegui.framework.core.api.annotation.OriginalResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    // 会被Advice包装
+    @GetMapping("/")
+    public String test(){
+        return "Test";
+    }
+
+    // 不被Advice包装
+    @GetMapping("/test")
+    @OriginalResponse
+    public String test2(){
+        return "Test2";
+    }
+}
