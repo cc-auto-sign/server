9d37fd7aa4d7d42549d36e6ed4f55dd3015cd2b6
WANG,ZHEN <admin@5ibug.net>
2025-07-02 14:48:35 +0800

    feat: 新增数据源

--- cc-admin-start/src/main/java/net/kegui/start/controller/TestController.java
+++ cc-admin-start/src/main/java/net/kegui/start/controller/TestController.java
@@ -1,11 +1,16 @@
 package net.kegui.start.controller;
 
 import net.kegui.framework.core.api.annotation.OriginalResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     // 会被Advice包装
     @GetMapping("/")
@@ -18,5 +23,11 @@ public class TestController {
     @OriginalResponse
     public String test2(){
         return "Test2";
+    }
+
+    // 测试db
+    @GetMapping("/db")
+    public Object testDb(){
+        return jdbcTemplate.queryForList("select 1;");
     }
 }
